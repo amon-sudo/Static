180d7c0e021137bb2135e9bcd0249e34a458e175
amon-sudo <amonogino@gmail.com>
2026-03-15 22:16:09 +0300

    statism

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template("home.hmtl")
+
+
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
+       
+    
