c2aca12fe73a684f18b1b6605089e8697c887ad5
amon-sudo <amonogino@gmail.com>
2026-03-16 21:09:45 +0300

    hard

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from second import second
 
 app = Flask(__name__)
+app.register_blueprint(second, url_prefix="")
 
-@app.route("/home")
+
 @app.route("/")
-def home():
-    return render_template("home.html")
+def test():
+    return "<h1>Test<h1>"
 
 
 
