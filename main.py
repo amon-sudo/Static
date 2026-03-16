11dcf85c8d54a208df56e4e636121da5677d20c3
amon-sudo <amonogino@gmail.com>
2026-03-16 22:47:32 +0300

    drf

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 from second import second
 
 app = Flask(__name__)
-app.register_blueprint(second, url_prefix="")
 
+app.register_blueprint(second, url_prefix="/second")
 
 @app.route("/")
 def test():
-    return "<h1>Test<h1>"
-
-
-
+    return "<h1>Test</h1>"
 
 if __name__ == "__main__":
-   app.run(debug=True)
-       
-    
+    app.run(debug=True)
